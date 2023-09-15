3403e88d367d9289fc2b44dca298bc597c55e610
SnowflakeGIT23 <145074804+SnowflakeGIT23@users.noreply.github.com>
2023-09-15 15:46:29 +1000

    get kiwi info

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,9 @@ streamlit.dataframe(fruits_to_show)
 #New Sct to dispay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 #streamlit.text(fruityvice_response.json())
 
 # normalise json response 
